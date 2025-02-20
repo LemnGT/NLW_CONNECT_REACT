59e7be0009e8fb922bb7b3ff08408e672943d7ed
Marcelo G <m2biel000@icloud.com>
2025-02-20 17:56:39 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { ArrowRight, Copy } from "lucide-react";
+import { Button } from "@/components/button";
+import { IconButton } from "@/components/icon-button";
+import { Input } from "@/components/input";
+
+export default function Home() {
+  return (
+    <main>
+      <Button>
+        Enviar
+        <ArrowRight />
+      </Button>
+      <IconButton>
+        <Copy />
+      </IconButton>
+      <div>
+        <Input placeholder="Digite seu e-mail" />
+      </div>
+    </main>
+  );
+}
---
+++ src/components/icon-button.tsx
@@ -0,0 +1,12 @@
+import { ReactNode, ComponentProps } from "react";
+
+interface IconProps extends ComponentProps<"button"> {}
+
+export function IconButton(props: IconProps) {
+  return (
+    <button
+      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      {...props}
+    />
+  );
+}
