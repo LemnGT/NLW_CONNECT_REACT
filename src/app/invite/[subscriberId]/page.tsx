08dc6a2b9c9c8ca8cd2e48070f615c4a8413e89d
Marcelo G <m2biel000@icloud.com>
2025-02-23 22:48:20 -0300

    using API

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image'
 import { Ranking } from './ranking'
 
-import logo from '../../assets/logo.svg'
+import logo from '../../../assets/logo.svg'
 import { InviteLinkInput } from './invite-link-input'
 import { Stats } from './stats'
 
-export default function InvitePage() {
-  const inviteLink = 'https://localhost:3000/invite/12243132'
+interface InvitePageProps {
+  params: Promise<{ subscriberId: string }>
+}
+
+export default async function InvitePage(props: InvitePageProps) {
+  const { subscriberId } = await props.params
+
+  const inviteLink = `http://localhost:3333/invites/${subscriberId}`
 
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
@@ -32,7 +38,7 @@ export default function InvitePage() {
             </p>
           </div>
           <InviteLinkInput inviteLink={inviteLink} />
-          <Stats />
+          <Stats subscriberId={subscriberId} />
         </div>
       </div>
       <Ranking />
